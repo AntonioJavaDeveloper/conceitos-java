dce2f6a84e06860c93baa2db9ea79429e3b74008
Antonio Gutierrez <antonio@javadeveloper.com.br>
2022-07-16 02:00:18 -0300

    12 - Tipos primitivos pt 03 - casting

--- src/br/com/javadeveloper/javabasico/introducao/Aula02TiposPrimitivos.java
+++ src/br/com/javadeveloper/javabasico/introducao/Aula02TiposPrimitivos.java
@@ -3,9 +3,11 @@ package br.com.javadeveloper.javabasico.introducao;
 public class Aula02TiposPrimitivos {
     public static void main(String[] args) {
         //byte, short, int, long, float, double, char
-        int idade = 10;
-        double salarioDouble = 2000; // 8 bytes
-        float salarioFloat = 2500; // 4 bytes
+        int idade = (int)1000000000;
+        long numeroGrande = 100000000L;
+        long numeroGrandeConvertido = (long)100000000.23;
+        double salarioDouble = 2000.0; // 8 bytes
+        float salarioFloat = (float)2500.0F; // 4 bytes
         byte idadeByte = 10; // -128 até 127
         short idadeShort = 10; // 2 bytes
         boolean verdadeiro = true; // 1 bit
@@ -14,5 +16,8 @@ public class Aula02TiposPrimitivos {
 
         System.out.println("A idade é" + idade + "anos");
         System.out.println(verdadeiro);
+        System.out.println(salarioFloat);
+        System.out.println(numeroGrande);
+        System.out.println(numeroGrandeConvertido);
     }
 }
